94a76674fa7620388dc0a26b271efc7605e302b1
Samuel <affum3331@gmail.com>
2024-10-03 17:04:40 +0000

    update

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
         },
     package_dir={"": "src"},
-packages=setuptools.find_packages(where="src")
+packages=setuptools.find_packages(where="src"),
 )
     
